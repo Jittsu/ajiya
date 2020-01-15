28cd28fc60d6bbaad49816e4a867171e4be3f8bb
Jittsu <naoto.j3781@gmail.com>
2020-01-15 15:26:01 +0900

    文字コードの修正

--- ajiya_gitshare.py
+++ ajiya_gitshare.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# $B$"$8$d$N$*J[Ev%a%K%e!<DLCN%7%9%F%`(B
-# line notify$B$r;HMQ(B
-# cron$B$rMQ$$$FKhD+(B9$B;~$K$3$N%U%!%$%k$,<B9T$5$l$k(B
-# crontab -l$B$GDj;~<B9T%U%!%$%k$N%j%9%H!"(Bcrontab -e$B$GJT=8$,2DG=(B
+# あじやのお弁当メニュー通知システム
+# line notifyを使用
+# cronを用いて毎朝9時にこのファイルが実行される
+# crontab -lで定時実行ファイルのリスト，crontab -eで編集が可能
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,14 +13,14 @@ import datetime
 
 def main():
 	weekday = datetime.date.today().weekday()
-	# $BEZF|$ODLCN$7$J$$(B ---
+	# 土日は通知しない ---
 	if weekday != 5 and weekday !=6:
 		detail = scraping()
 		send_line(detail)
 	else:
 		pass
 
-# $B%9%/%l%$%T%s%0(B ---
+# スクレイピング ---
 def scraping() -> str:
 	r = requests.get('http://ajiya1.com/archives/category/higawari')
 	soup = BeautifulSoup(r.content, 'html.parser')
@@ -29,12 +29,12 @@ def scraping() -> str:
 	img_url = thumb_img.get('src')
 	img = requests.get(img_url)
 
-	# $B%G%#%l%/%H%j$,$"$k$H$-$O$=$3$KJ]B8(B ---
+	# ディレクトリがある時はそこに保存 ---
 	try:
 		with open('[PATH TO ajiya]/data/' + re.split('[/]', img_url)[-1], 'wb') as f:
 			f.write(img.content)
 
-	# $BL5$$>l9g:n@.(B ---
+	# 無い場合作成 ---
 	except FileNotFoundError:
 		os.mkdir('[PATH TO ajiya]/data/')
 		with open('[PATH TO ajiya]/data/' + re.split('[/]', img_url)[-1], 'wb') as f:
@@ -42,14 +42,14 @@ def scraping() -> str:
 
 	return details.text
 
-# Line Notify API$B$X$N(BPOST ---
+# Line Notify APIへのPOST ---
 def send_line(detail: str):
 	url = 'https://notify-api.line.me/api/notify'
 	token = 'Your Token'
 	headers = {'Authorization': 'Bearer ' + token}
 	payload = {'message' :  detail[:-2]}
 
-	# $B2?$,0c$&$s$+CN$i$s$1$I(Bjpg$B$8$c$J$/$F(Bjpeg$B$i$7$$$N$GJQ49(B ---
+	# 何が違うんか知らんけどjpgじゃなくてjpegらしいので変換 ---
 	f = glob.glob('[PATH TO ajiya]/data/*.jpg')
 	f = f[0]
 
@@ -64,7 +64,7 @@ def send_line(detail: str):
 	# POST ---
 	r = requests.post(url, headers=headers, params=payload, files=files)
 
-	# $B<L??$$$i$s$N$G:o=|(B(PATH$B$r4V0c$($k$H;`$L$N$G5$$r$D$1$k(B) ---
+	# 写真いらんので削除(PATHを間違えると死ぬので気をつける) ---
 	command = 'rm -rf [PATH TO ajiya]/data/*'
 	os.system(command)
 
